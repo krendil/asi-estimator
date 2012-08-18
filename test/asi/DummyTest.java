2f62d49ad86131b493f4e8d08a34b60f36588be3
David Osborne <krendil@localhost.localdomain>
2012-08-19 03:28:38 +1000

    Added dummy test so I can make sure the integration server runs it

---
+++ test/asi/DummyTest.java
@@ -0,0 +1,17 @@
+package asi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	/**
+	 * Dummy test to make sure the build system/integration server works
+	 */
+	@Test
+	public void test() {
+		assertEquals(2,  1 + 1);
+	}
+
+}
