bc2358b4f4db48403b184d11a1a8a0b265d0b693
David Osborne <krendil@localhost.localdomain>
2012-08-19 03:16:06 +1000

    Initial commit - empty Google app engine project

---
+++ src/asi/client/Asi_estimator.java
@@ -0,0 +1,17 @@
+package asi.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Asi_estimator implements EntryPoint {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		
+	}
+}
