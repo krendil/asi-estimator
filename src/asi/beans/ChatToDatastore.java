66969f0b65c951e51024b3d3ddd4f16263319c42
Steven Turner <shkibr@hotmail.com>
2012-09-17 15:23:42 +1000

    Datastore is working

--- src/asi/beans/ChatToDatastore.java
+++ src/asi/beans/ChatToDatastore.java
@@ -1,16 +1,27 @@
 package asi.beans;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Transaction;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
 
 public class ChatToDatastore {
+	
+	
+	private static final Logger logger = Logger.getLogger(ChatToDatastore.class.getCanonicalName());
 	private static DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-	private final String HISTORY = "History";
-	private final String HISTORY_CONTENT = "Content";
+	private static MemcacheService keycache = MemcacheServiceFactory.getMemcacheService();
+	
+	private static final String HISTORY = "History";
+	private static final String HISTORY_CONTENT = "Content";
 	
 	
 
@@ -20,12 +31,14 @@ public class ChatToDatastore {
 	 * @return
 	 * @throws EntityNotFoundException 
 	 */
-	public String dsLoadHistory( String index ) throws EntityNotFoundException {
+	public static String dsLoadHistory( String index ) throws EntityNotFoundException {
+		
+		logger.log(Level.INFO, "Search the entity");
 		
 		Key key = KeyFactory.createKey( HISTORY , index );
 		
 		Entity entity = datastore.get( key );
-		
+
 		return entity.getProperty( HISTORY_CONTENT ).toString();
 		
 //		return "dbLoad called with \""+index+"\".";
@@ -38,7 +51,7 @@ public class ChatToDatastore {
 	 * @param content
 	 * @return
 	 */
-	public String dsSaveHistory(String content) {
+	public static String dsSaveHistory(String content) {
 		
 		Entity results = new Entity( HISTORY );
 		
@@ -46,11 +59,41 @@ public class ChatToDatastore {
 		
 		datastore.put( results );
 		
+		logger.log(Level.INFO, "Saving entity");
+		    
+		Key key = results.getKey();
+	    Transaction txn = datastore.beginTransaction();
+	    try {
+	      datastore.put(results);
+		  txn.commit();
+	    } finally {
+		  if (txn.isActive()) {
+		    txn.rollback();
+		  } else {
+			addToCache(key, results);
+		  }
+	    }
+		
 		return results.getKey().toString();
 		
 //		return "dbLoad called with \""+content+"\".";
 
 	}
 	
+	
+	
+	/**
+	 *  *** written by google ***
+	 * Adds the entity to cache
+	 * 
+	 * @param key
+	 *          : key of the entity
+	 * @param entity
+	 *          : Entity being added
+	 */
+  public static void addToCache(Key key, Entity entity) {
+    logger.log(Level.INFO, "Adding entity to cache");
+    keycache.put(key, entity);
+  }
 	
 }
--- src/asi/server/GetHistoryServlet.java
+++ src/asi/server/GetHistoryServlet.java
@@ -6,24 +6,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.Enumeration;
 
-
-import javax.servlet.ServletInputStream;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import asi.beans.ChatToDatastore;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
 
 
 public class GetHistoryServlet extends HttpServlet  {
@@ -77,9 +67,7 @@ public class GetHistoryServlet extends HttpServlet  {
 	private String getXML(String key) throws EntityNotFoundException {
 		String xml = null;
 		
-		ChatToDatastore datastore = new ChatToDatastore();
-		
-		xml = datastore.dsLoadHistory( key );
+		xml = ChatToDatastore.dsLoadHistory( key );
 		
 		return xml;
 	}
