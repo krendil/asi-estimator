f0d89fb753b7aeb95210bacad4a78fb72f5672b5
unknown <n7533934@LAB-GPS502L-32.qut.edu.au>
2012-09-18 14:46:46 +1000

    Faulty xml generation

--- src/asi/client/Asi_estimator.java
+++ src/asi/client/Asi_estimator.java
@@ -1,13 +1,18 @@
 package asi.client;
 
+//import com.example.myproject.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.InlineHTML;
@@ -77,8 +82,130 @@ public class Asi_estimator implements EntryPoint {
 		
 	    
 	    RootPanel.get("interface").add(webGui.tabPanel);
-	    
+	 // Create a handler for the sendButton and nameField
+		class MyHandler implements ClickHandler {
+			/**
+			 * Fired when the user clicks on the sendButton.
+			 */
+			public void onClick(ClickEvent event) {
+				sendNameToServer();
+			}
+
+
+			/**
+			 * Send the name from the nameField to the server and wait for a response.
+			 */			private void sendNameToServer() {
+					// First, we validate the input.
+					
+					String textToServer = generateXML();//powerGenerationField.getText(); //Generate XML using this input
+					
+					//would be nice to have verification here
+
+					// Then, we send the input to the server.
+					webGui.calculateButton.setEnabled(false);
+					
+					RequestBuilder request = new RequestBuilder(RequestBuilder.POST, 
+							/*   //<-- Comment toggler, add leading / to enable first section
+							"http://asi-estimator.appspot.com/asi_estimator/estimate"
+							/*/
+							"http://127.0.0.1:8888/asi_estimator/estimate"
+							//*/
+					);
+					
+					request.setRequestData(textToServer);//Change to xml string
+					
+					
+					request.setCallback(new RequestCallback(){
+
+						@Override
+						public void onResponseReceived(Request request,
+								Response response) {
+							String responseText = response.getText();
+							
+							Document doc = XMLParser.parse(responseText);
+							
+							Node powerTag = doc.getElementsByTagName("power").item(0);
+							Node revenueTag = doc.getElementsByTagName("revenue").item(0);
+							
+							String powerString = powerTag.getFirstChild().getNodeValue();
+							String revenueString = revenueTag.getFirstChild().getNodeValue();
+							
+							String resultString = "Power = " + powerString + " kWh</br> Revenue = $" + revenueString;
+							InlineLabel resultsLabel = new InlineLabel(resultString);
+							webGui.resultsPanel.add(resultsLabel);						
+							
+						}
+
+						@Override
+						public void onError(Request request, Throwable exception) {
+							// TODO Auto-generated method stub
+							
+						}
+						
+					} );
+					
+					try {
+						request.send();
+					} catch (RequestException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			//create handlers for widgets
+				//Submit
+			//add handlers to widgets
+			webGui.calculateButton.addClickHandler(new MyHandler());
+			
+
 	}
+	private String generateXML() {
+		//FIXME Obviously, this is bogus
+		return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
+			"<!DOCTYPE solarquery SYSTEM \"http://asi-estimator.appspot.com/solarquery.dtd\">"+
+			"<solarquery>"+
+			"	<array>"+
+			"		<bank facing=\""+getFacing(webGui.panelDirection.getItemText(webGui.panelDirection.getSelectedIndex())) +
+				"\" number=\""+webGui.nPanels+"\" power=\""+webGui.panelPower+
+				"\" tilt=\""+(String)webGui.panelAngle.getItemText(webGui.panelAngle.getSelectedIndex())+"\"" +
+						" price=\""+webGui.panelCost+"\"/>"+
+			"	</array>"+
+			"	<feedin rate=\""+webGui.feedInTariff+"\" />"+
+			"	<consumption power=\""+webGui.powerConsumption+"\" rate=\""+webGui.tariffRates+"\"/>" +
+			"	<sunlight hours=\""+webGui.hoursOfSun+"\" />" +
+			"	<inverter efficiency=\""+webGui.inverterEfficiency+"\" price=\""+webGui.inverterCost+"\" />"+
+			"</solarquery>";
+	}
+	private String getFacing(String cardin) {
+		if(cardin== "N"){
+			return "0";
+		}
+		else if(cardin== "NE"){
+			return "45";
+		}
+		else if(cardin== "E"){
+			return "90";
+		}
+		else if(cardin== "SE"){
+			return "135";
+		}
+		else if(cardin== "S"){
+			return "180";
+		}
+		else if(cardin== "SW"){
+			return "225";
+		}
+		else if(cardin== "W"){
+			return "270";
+		}
+		else if(cardin== "NW"){
+			return "315";
+		}
+		else{return "0";}
+	}
+
+
+	
 }
 	
 	
